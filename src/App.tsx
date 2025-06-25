acb5d6f0e587037ce881e127f4632d52b2f1ed80
hugohf05 <145617882+hugohf05@users.noreply.github.com>
2025-06-25 22:54:24 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
-import { Sonner } from "@/components/ui/sonner";
+//import { Sonner } from "@/components/ui/sonner";
 
 import Index from "@/pages/Index";
 import CasoExito from "@/pages/CasoExito";
