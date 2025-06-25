ab588e323127657fb1c796520eb9a2f5e1d5d0fd
hugohf05 <145617882+hugohf05@users.noreply.github.com>
2025-06-25 23:00:08 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
-//import { Sonner } from "@/components/ui/sonner";
+import { Sonner } from "@/components/ui/sonner";
 
 import Index from "@/pages/Index";
 import CasoExito from "@/pages/CasoExito";
@@ -17,7 +17,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      //<Sonner />
+      <Sonner />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
